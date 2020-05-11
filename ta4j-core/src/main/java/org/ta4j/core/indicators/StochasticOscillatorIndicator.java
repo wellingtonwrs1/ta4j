9ccef143f5b057e55b40dcf902ec1c67c3f4a5aa
Wellington Rodrigues <wellington.rodrigues@algosoft.com.br>
2020-05-11 12:25:29 -0300

    .

---
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/StochasticOscillatorIndicator.java
@@ -0,0 +1,58 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2014-2017 Marc de Verdelhan, 2017-2019 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators;
+
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Indicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * Stochastic oscillator D.
+ * <p>
+ * Receive {@link StochasticOscillatorKIndicator} and returns its
+ * {@link SMAIndicator SMAIndicator(3)}.
+ */
+public class StochasticOscillatorIndicator extends CachedIndicator<Num> {
+
+    private Indicator<Num> indicator;
+
+    public StochasticOscillatorIndicator(BarSeries barSeries, int k, int d) {
+        this(new SMAIndicator(new StochasticOscillatorKIndicator(barSeries, k), d));
+    }
+
+    public StochasticOscillatorIndicator(Indicator<Num> indicator) {
+        super(indicator);
+        this.indicator = indicator;
+    }
+
+    @Override
+    protected Num calculate(int index) {
+        return indicator.getValue(index);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " " + indicator;
+    }
+}
