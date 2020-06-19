5a227bb696d72c033624fa830f26a5ec431d5da1
Wellington Rodrigues <wellington.rodrigues@algosoft.com.br>
2020-06-19 08:00:46 -0300

    .

--- ta4j-core/src/main/java/org/ta4j/core/trading/rules/StopLossRule.java
+++ ta4j-core/src/main/java/org/ta4j/core/trading/rules/StopLossRule.java
@@ -135,7 +135,7 @@ public class StopLossRule extends AbstractRule {
     private boolean calculatePips(Num entryPrice, Num currentPrice, boolean isSell) {
         BigDecimal entry = ((BigDecimal) entryPrice.getDelegate());
         BigDecimal current = ((BigDecimal) currentPrice.getDelegate());
-        int position = pipPosition > 0 ? pipPosition : current.scale();
+        int position = pipPosition > 0 ? pipPosition : entry.scale();
         if (isSell) {
             return current.subtract(entry).movePointRight(position).compareTo((BigDecimal) lossValue.getDelegate()) >= 0;
         }
